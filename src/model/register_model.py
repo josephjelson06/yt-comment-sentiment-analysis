19d3904b98b9f2058eebe3d79b34ff7779a09ea3
Jelson Joseph <josephjelson06@gmail.com>
2025-07-11 22:07:50 +0530

    New Tracking URI

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri("http://ec2-3-95-155-151.compute-1.amazonaws.com:5000/")
+mlflow.set_tracking_uri("http://ec2-54-147-203-211.compute-1.amazonaws.com:5000/")
 
 
 # logging configuration
